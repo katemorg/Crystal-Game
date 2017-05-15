62baed15611265616338c33d42303ca7666fc80c
Kate Morgenstern <katemorg51@gmail.com>
2017-05-14 22:13:31 -0400

    initial framework

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+//variables: 
+
+/*variables:
+
+
+*/
+
+/*on click event to start
+	generates a random number to score, saves as variable
+	generates random numbers for each crystal and saves as variables
+*/
+
+//on click event to reset, if player wants to quit and restart the game
+
+//
